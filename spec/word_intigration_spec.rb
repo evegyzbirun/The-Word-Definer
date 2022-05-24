c270b5026e03bcc37f569a66985d231b3c39564f
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-05-24 15:07:10 -0700

    fix path to words.

--- spec/word_intigration_spec.rb
+++ spec/word_intigration_spec.rb
@@ -1,25 +1,26 @@
+
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('create a word path', {type => :feature}) do 
+describe('create a word path', {:type => :feature}) do 
   it('creates a word and then goes to the word list page') do
     visit("/words")
     click_on('Add a new word')
-    fill_in('word', :with => 'good')
+    fill_in('word_name', :with => 'good')
     click_on('Go!')
     expect(page).to have_content('good')
   end
 end
 
-describe('create a difinition path', {:type => :feature}) do
+describe('create a definition path', {:type => :feature}) do
   it('creates a word and then goes to word page') do 
     word = Word.new("run", nil)
     word.save
     visit("/words/#{word.id}")
     fill_in('definition_name', :with => 'moving fast')
-    click_on('Add a definition:')
+    click_on('Add definition')
     expect(page).to have_content('moving fast')
   end
 end
